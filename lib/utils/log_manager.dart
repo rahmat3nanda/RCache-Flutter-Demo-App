fe9913722bd35ec6658f08b3ff412d00ed2021dd
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-04 22:25:07 +0700

    Create log manager

---
+++ lib/utils/log_manager.dart
@@ -0,0 +1,85 @@
+import 'dart:convert';
+
+import 'package:intl/intl.dart';
+import 'package:rcache_demo_flutter/app_rcache_key.dart';
+import 'package:rcache_flutter/rcache.dart';
+
+enum LogActionType {
+  addKey,
+  removeKey,
+  save,
+  read,
+  remove,
+  clear;
+
+  String get value {
+    switch (this) {
+      case LogActionType.addKey:
+        return "Add Key";
+      case LogActionType.removeKey:
+        return "Remove Key";
+      case LogActionType.save:
+        return "Save";
+      case LogActionType.read:
+        return "Read";
+      case LogActionType.remove:
+        return "Remove";
+      case LogActionType.clear:
+        return "Clear";
+    }
+  }
+}
+
+class LogModel {
+  final String? action;
+  final String? value;
+  final String? time;
+
+  LogModel({
+    this.action,
+    this.value,
+    this.time,
+  });
+
+  factory LogModel.fromJson(Map<String, dynamic> json) => LogModel(
+        action: json["action"],
+        value: json["value"],
+        time: json["time"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "action": action,
+        "value": value,
+        "time": time,
+      };
+}
+
+class LogManager {
+  static LogManager? _manager;
+
+  factory LogManager() => _manager ??= LogManager._internal();
+
+  LogManager._internal();
+
+  static LogManager get shared => _manager ??= LogManager._internal();
+
+  List<LogModel> _localData = [];
+
+  Future<List<LogModel>> data() async {
+    String? s = await RCache.common.readString(key: AppRCacheKey.logs);
+    if (s != null) {
+      List<LogModel> data =
+          List<LogModel>.from(jsonDecode(s).map((x) => LogModel.fromJson(x)));
+      _localData = data;
+    }
+    return Future.value(_localData);
+  }
+
+  Future<void> add({
+    required LogActionType action,
+    required String value,
+  }) async {
+    String time = DateFormat("d-M-yyyy HH:mm:ss").format(DateTime.now());
+    _localData.add(LogModel(action: action.value, value: value, time: time));
+  }
+}
